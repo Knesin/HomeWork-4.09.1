44020f476bbb05fc7ce36f364c339f0bbe83c7fe
Maksim Kl <GuruHacker@yandex.ru>
2023-06-12 19:52:39 +0300

    Initial commit

---
+++ HomeWork-4.09.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include "move_vector.h"
+
+
+int main()
+{
+    std::vector <std::string> one = { "test_string1", "test_string2" };
+    std::vector <std::string> two;
+    move_vectors(one, two);
+    for (const auto& i : two) {
+        std::cout << i << std::endl;
+    }
+}
